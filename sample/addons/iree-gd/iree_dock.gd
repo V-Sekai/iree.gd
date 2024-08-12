637bec22bc2283abc074c7ab9304e093c4e1242a
Dragos Daian <daian.dragos@yahoo.com>
2024-08-12 10:36:20 +0200

    update123

--- sample/addons/iree-gd/iree_dock.gd
+++ sample/addons/iree-gd/iree_dock.gd
@@ -5,9 +5,9 @@ var dock
 
 func _enter_tree():
 	dock = preload("res://addons/iree-gd/iree_dock.tscn").instantiate()
-	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
+	add_control_to_bottom_panel(dock, "IREE Models")
 
 
 func _exit_tree():
-	remove_control_from_docks(dock)
+	remove_control_from_bottom_panel(dock)
 	dock.free()
